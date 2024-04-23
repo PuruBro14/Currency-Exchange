20d962a62d7f8b89823443f6957ffcec9eed44c5
Anuj Singh Nainwal <anujsinghnainwal@gmail.com>
2024-04-23 23:12:10 +0530

    update code and added server url

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -1,16 +1,18 @@
-const BASE_URL="http://localhost:8080/api/v1"
+// const BASE_URL="http://localhost:8080/api/v1"
+// production url
+const BASE_URL = "http://16.170.209.126:9090/api/v1";
 
-export const endpoints={
-    SIGNUP_API:BASE_URL+"/auth/signup",
-    LOGIN_API:BASE_URL+"/auth/login"
-}
+export const endpoints = {
+  SIGNUP_API: BASE_URL + "/auth/signup",
+  LOGIN_API: BASE_URL + "/auth/login",
+};
 
 export const bookOrderEndpoints = {
   CREATE_ORDER: BASE_URL + "/createOrder",
   GET_ORDERS: BASE_URL + "/fetchAllBookOrders",
-  DELETE_ORDERS:BASE_URL+"/deleteOrderById"
+  DELETE_ORDERS: BASE_URL + "/deleteOrderById",
 };
 
-export const contactUsEndPoint={
-  CONTACT_US_API:BASE_URL+"/contact-us"
-}
+export const contactUsEndPoint = {
+  CONTACT_US_API: BASE_URL + "/contact-us",
+};
